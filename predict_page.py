439b326cc2009eb12d457d1f0ca6afa04960beba
Adegoke Israel <47308654+adegokeisrael@users.noreply.github.com>
2024-02-08 10:32:10 +0100

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pickle5 as pickle
+import pickle
 import numpy as np
 
 
@@ -10,9 +10,9 @@ def load_model(name):
 
 
 def show_predict_page():
-    st.title("Livestock Disease Prediction")
+    st.title("Livestock Disease Diagnosis")
 
-    st.write("""### Give some information so we can predict Livestock Disease """)
+    st.write("""### Give some information so we can predict Livestock Disease""")
 
     Symptoms1 = (
         'loss of appetite', 'depression','shortness of breath',
@@ -52,10 +52,10 @@ def show_predict_page():
     )
 
     animal_temperatures = {
-        "goat": "Normal body temperature of a healthy goat: 101.5°F to 104°F (38.6°C to 40°C)",
-        "cow": "Normal body temperature of a healthy cow: 100.4°F to 102.5°F (38°C to 39.2°C)",
-        "sheep": "Normal body temperature of a healthy sheep: 101.5°F to 104°F (38.6°C to 40°C)",
-        "buffalo": "Normal body temperature of a healthy buffalo: 100.4°F to 103.1°F (38°C to 39.5°C)"
+        "Goat": "Normal body temperature of a healthy goat: 101.5°F to 104°F (38.6°C to 40°C)",
+        "Cow": "Normal body temperature of a healthy cow: 100.4°F to 102.5°F (38°C to 39.2°C)",
+        "Sheep": "Normal body temperature of a healthy sheep: 101.5°F to 104°F (38.6°C to 40°C)",
+        "Buffalo": "Normal body temperature of a healthy buffalo: 100.4°F to 103.1°F (38°C to 39.5°C)"
     }
 
     livestock = st.selectbox("Select Livestock", list(animal_temperatures.keys()))
@@ -69,15 +69,40 @@ def show_predict_page():
     st.title("Animal Body Temperature Hint Box")
 
     
-    Temperature = st.slider("Select a Animal Body Temperture", min_value=0, max_value=120)
+    Temperature = st.slider("Select a Animal Body Temperture (°F)", min_value=100, max_value=105)
 
     hint_text = animal_temperatures.get(livestock, "Select an animal to see its normal body temperature.")
     st.info(hint_text)
 
-    
+    prescription={"Goat_anthrax":"Isolate and mark the affected goat.Disinfect the area thoroughly and avoid handling the goat or its body.Contact a veterinarian and report the suspected case to local authorities, as anthrax is a zoonotic disease that can affect humans too.",
+"Goat_blackleg":"Isolate the affected goat to prevent further spread. This disease kills within 12- 48 hours of infection Contact your veterinarian immediately.",
+"Goat_pneumonia":"Isolate the affected goat and monitor its breathing. Provide a clean and dry environment with proper ventilation to keep the sheep warm and dry.Feed the goat with balanced diet that provides enough calories to maintain body temperature.",
+"Goat_lumpy virus":"Isolate the infected goat to prevent the spread of the virus. Clean the area around the wound with mild soap and water. Maintain good hygiene and disinfect the environment to reduce the risk of transmission.",
+"Goat_foot and mouth":"Isolate the infected goat to prevent disease spread. Clean the area around the blister with mild soap and water. Check the blister regularly for signs of infection such as redness or discharge.",
 
 
-    age = st.slider("Age of livestock", 0, 15, 3) 
+"Buffalo_anthrax":"Isolate and mark the affected buffalo. Disinfect the area thoroughly and avoid handling the buffalo or its body. Contact a veterinarian and report the suspected case to local authorities, as anthrax is a zoonotic disease that can affect humans too.",
+"Buffalo_blackleg":"Isolate the affected buffalo to prevent further spread. Contact a veterinarian immediately for support as diseases kill within 48 hours.",
+"Buffalo_pneumonia":"Isolate the affected buffalo and monitor its breathing. Provide a clean and dry environment with proper ventilation to keep the sheep warm and dry. Provide supportive care like fluid therapy and antibiotics.",
+"Buffalo_lumpy virus":"Isolate the buffalo to prevent the spread of the virus. Clean the area around the wound with mild soap and water. Maintain good hygiene and disinfect the environment to reduce the risk of transmission.",
+"Buffalo_foot and mouth":"Isolate the infected buffalo to prevent disease spread. Clean the area around the blister with mild soap and water. Treat the blister by regularly dressing.",
+
+"Cow_anthrax":"Isolate and mark the affected cow. Disinfect the area thoroughly and avoid handling the cow or its body. Contact a veterinarian and report the suspected case to local authorities, as anthrax is zoonotic disease that can affect humans too.",
+"Cow_blackleg":"Isolate the affected cow to prevent spreading the disease. Contact a veterinarian immediately for support as diseases kill within 48 hours.",
+"Cow_pneumonia":"Isolate the affected cow to prevent disease spread to other animals. Provide a well-ventilated, dry, and comfortable environment. Keep the animal warm and ensure access to clean water and food.",
+"Cow_lumpy virus":"Isolate the affected cow to prevent disease spread. Clean the area around the wound with mild soap and water. Practice strict biosecurity measures, including disinfection of equipment and personnel.",
+"Cow_foot and mouth":"Isolate the affected cow to avoid disease spread. Clean the area around the blister with mild soap and water. Check the blister regularly for signs of infection such as redness or discharge. ",
+
+"Sheep_anthrax":"Isolate and mark the affected cow. Disinfect the area thoroughly and avoid handling the cow or its body. Contact a veterinarian and report the suspected case to local authorities, as anthrax is zoonotic disease that can affect humans too.",
+"Sheep_blackleg":"Isolate the affected sheep to prevent further spread. This disease kills within 12- 48 hours of infection Contact your veterinarian immediately.",
+"Sheep_pneumonia":"Isolate the affected sheep and monitor its breathing. Provide a clean and dry environment with proper ventilation to keep the sheep warm and dry. Provide supportive care like fluid therapy and antibiotics.",
+"Sheep_lumpy virus":"Isolate the infected sheep to prevent the spread of the virus. Clean the area around the wound with mild soap and water. Maintain good hygiene and disinfect the environment to reduce the risk of transmission.",
+"Sheep_foot and mouth":"Isolate the infected sheep to prevent disease spread. Clean the area around the blister with mild soap and water.  Apply wound healing cream on blister surface. "
+
+}
+
+
+    age = st.slider("Age of livestock (Years)", 0, 15, 3) 
     
     prognosis_list_cow=['pneumonia', 'blackleg', 'lumpy virus', 'anthrax',
        'foot and mouth']
@@ -88,17 +113,15 @@ def show_predict_page():
     prognosis_list_goat= ['foot and mouth', 'anthrax', 'lumpy virus', 'pneumonia',
        'blackleg']
     
-    
-
-    ok = st.button("Predict Disease in Livestock")
-
     data = load_model(f'xgmodel{livestock}.pkl')
 
     le_symptom1 = data["le_symptom1"]
     le_symptom2 =data["le_symptom2"]
-    le_symptom3 = data["le_symptom2"]
+    le_symptom3 = data["le_symptom3"]
     regressor = data["xgmodel"]
     label_encoder =data["label_encoder"]
+
+    ok = st.button("Predict Disease in Livestock")
     if ok:
         x=np.array([[age,Temperature,First_Symptoms,Second_Symptoms ,Third_Symptoms]])
         x[:, 2] = le_symptom1.transform(x[:,2])
@@ -131,3 +154,6 @@ def show_predict_page():
 
 
         st.subheader(f"The predicted prognosis for this {livestock } is {prognosis}")
+        key=f"{livestock}_{prognosis}"
+        
+        st.subheader(f"RECOMMENDATION: {prescription[key] } ")
